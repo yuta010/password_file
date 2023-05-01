508c1d138c712e7c252e3aee3e66cc84ec9f712a
yuta010 <kuromame.guri@gmail.com>
2023-05-01 07:22:16 +0000

    password_managerの作成

---
+++ password_manager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "パスワードマネージャーへようこそ！"
+echo "サービス名を入力してください："
+read service_name
+echo "ユーザー名を入力してください："
+read user_name
+echo "パスワードを入力してください："
+read -s password 
+echo "Thank you!"
+
+echo "$service_name:$user_name:$password" >>password.txt
